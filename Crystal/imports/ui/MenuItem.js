9f3d88d9a8e9ddfb45c3757e2964bc0be5d23bd4
trinity <trinity@Unknown-b8-8d-12-17-ec-48-2.lan>
2018-09-13 18:02:57 +0100

    adding bootstrap

--- Crystal/imports/ui/MenuItem.js
+++ Crystal/imports/ui/MenuItem.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class MenuItem extends Component{
     render(){
         return (
-             <li id={this.props.id}>
-                <a href={this.props.url}>{this.props.name}</a>
+             <li id={this.props.id} key={this.props.id} className="nav-item">
+                <a className="nav-link" href={this.props.url}>{this.props.name}</a>
              </li>
         )
           
